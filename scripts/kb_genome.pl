fbf0e53ce78aa43aa62b1a261c22b8d9c5349021
Scott Devoid <sdevoid@gmail.com>
2012-10-03 13:41:29 -0500

    Add kb_* scripts to reflect ms scripts.

---
+++ scripts/kb_genome.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Bio::KBase::fbaModel::CLI;
+my $serv = Bio::KBase::fbaModel::CLI->new("http://kbase.us/services/fbaModelCLI");
+
+my @args = @ARGV;
+unshift "genome", @args;
+my $stdin;
+if ( ! -t STDIN ) {
+    local $/;
+    $stdin = <STDIN>;
+}
+my ($status, $stdout, $stderr) = $serv->execute_command(\@args, $stdin);
+print STDERR $stderr;
+print STDOUT $stdout;
+exit($status);
