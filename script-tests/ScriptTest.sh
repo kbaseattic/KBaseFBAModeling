111ccca8c95c15d1aceb6bbadb6be1241b5d0927
Christopher Henry <chenry@mcs.anl.gov>
2013-01-26 11:47:50 -0600

    Fixing script tests so they work. I think it should be save and is advisable to run these against the dev workspace.

--- script-tests/ScriptTest.sh
+++ script-tests/ScriptTest.sh
@@ -1,11 +1,14 @@
-kbws-url "http://localhost:7058"
+kbws-url "http://bio-data-1.mcs.anl.gov/services/fba_gapfill"
 kbws-login kbasetest -p "@Suite525"
+kbws-deletews scripttest n
 kbws-createws scripttest n
 kbws-workspace scripttest
 kbfba-url "http://localhost:7036"
 kbfba-loadgenome "kb|g.0" -e
-kbfba-buildfbamodel "kb|g.0" -m "kb|g.0.fbamdl.201"
-kbfba-runfba "kb|g.0.fbamdl.201"
-kbfba-gapfill "kb|g.0.fbamdl.201"
-kbfba-addmedia myMedia "A;B;C"
-kbfba-gapgen "kb|g.0.fbamdl.201" -r myMedia --refmediaws scripttest
+kbfba-buildfbamodel "kb|g.0" -m "kb|g.0.fbamdl.201" -e
+kbfba-runfba "kb|g.0.fbamdl.201" -e
+kbfba-addmedia GlucoseMinimal "D-Glucose;Sulfate;Phosphate;NH3;Al;Boron;Ca2+;Cl-;Co2+;Cu2+;Fe2+;Fe3+;H+;H2O;K+;Mg;Mn2+;Ni2+;Molybdate;Na+;O2;Zn2+" -d -type "Minimal"
+#Gapfilling jobs run a long time and consume our cluster, which is busy doing work for the demo: I'm cutting this test for now. We need a test that makes a very fast gapfilling job. I know how to do this, but it will take time to develop the test.
+#kbfba-gapfill "kb|g.0.fbamdl.201" -e
+#kbfba-gapfill "kb|g.0.fbamdl.201" -m "GlucoseMinimal" --mediaws "scripttest" -e
+#kbfba-gapgen "kb|g.0.fbamdl.201" -r myMedia --refmediaws scripttest
