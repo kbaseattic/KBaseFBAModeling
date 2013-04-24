1414e49cb4867c9346382373189d5149deddea9a
Christopher Henry <chenry@mcs.anl.gov>
2013-04-24 02:43:47 -0500

    Fixnig authentication problem

--- lib/Bio/KBase/fbaModelServices/Impl.pm
+++ lib/Bio/KBase/fbaModelServices/Impl.pm
@@ -205,7 +205,8 @@ sub _authenticate {
 			Bio::KBase::Exceptions::KBaseException->throw(error => $token,
 			method_name => '_setContext');
 		}
-		my $split = [split(/\s/,$token)];
+		$token =~ s/\s/\t/;
+		my $split = [split(/\t/,$token)];
 		print "Logged user:".$split->[0]."\n";
 		return {
 			authentication => $token,
