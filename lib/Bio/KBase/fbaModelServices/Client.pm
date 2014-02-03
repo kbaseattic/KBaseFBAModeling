67a4b7209630642247493b2e5c9d042c0e5042fd
Christopher Henry <chenry@mcs.anl.gov>
2014-02-03 11:09:10 -0600

    Fixing client

--- lib/Bio/KBase/fbaModelServices/Client.pm
+++ lib/Bio/KBase/fbaModelServices/Client.pm
@@ -83,15 +83,15 @@ sub new
     #
     # We create an auth token, passing through the arguments that we were (hopefully) given.
 
-    {
-	my $token = Bio::KBase::AuthToken->new(@args);
+    #{
+	#my $token = Bio::KBase::AuthToken->new(@args);
 	
-	if (!$token->error_message)
-	{
-	    $self->{token} = $token->token;
-	    $self->{client}->{token} = $token->token;
-	}
-    }
+	#if (!$token->error_message)
+	#{
+	    $self->{token} = @args[0];
+	    $self->{client}->{token} = @args[0];
+	#}
+    #}
 
     my $ua = $self->{client}->ua;	 
     my $timeout = $ENV{CDMI_TIMEOUT} || (30 * 60);	 
