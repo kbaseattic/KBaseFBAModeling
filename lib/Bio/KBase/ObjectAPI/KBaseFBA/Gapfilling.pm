b70155be1abdeda40010863068a46c715806c293
root <root@mattb-dev.novalocal>
2014-03-03 22:04:16 +0000

    Further fixes to iterative gap fill. I added the blank gap fill solution to the end if no lines with reaction IDs were found but UNNEEDED was. I think that accomplishes the intended purpose without breaking things.

--- lib/Bio/KBase/ObjectAPI/KBaseFBA/Gapfilling.pm
+++ lib/Bio/KBase/ObjectAPI/KBaseFBA/Gapfilling.pm
@@ -463,93 +463,98 @@ sub createSolutionsFromArray {
 	my $mdl = $args->{model};
 	my $bio = $mdl->template()->biochemistry();
 	my $line;
+    my $has_unneeded = 0;
 	for (my $i=(@{$data}-1); $i >= 0; $i--) {
 		my $array = [split(/\t/,$data->[$i])];
 		if ($array->[1] =~ m/rxn\d+/) {
 			$line = $i;
 			last;
 		}
+		# Keep this separate becasue otherwise iterative gap fill breaks.
+		# If we find UNNEEDED and lines with reactions in them we only care about the latter.
+		if ( $array->[1] =~ m/UNNEEDED/ ) {
+		    $has_unneeded = 1;
+		}
 	}
 	my $solcount = @{$self->gapfillingSolutions()};
 	if (defined($line)) {
 		my $array = [split(/\t/,$data->[$line])];
-		if ($array->[1] eq "UNNEEDED") {
-			my $gfsolution = $self->add("gapfillingSolutions",{id => $self->id().".gfsol.".$solcount});
-			return;
-		} else {
-			my $solutionsArray = [split(/\|/,$array->[1])];   				
-			my $gfsolution;
-			my $count = 0;
-			for (my $k=0; $k < @{$solutionsArray}; $k++) {
-				if (length($solutionsArray->[$k]) > 0) {
-					my $rxnHash;
-					if ($k == 0 || $self->completeGapfill() ne "1") {
-						if (defined($gfsolution)) {
-							$gfsolution->solutionCost($count);
-						}
-						$count = 0;
-						$solcount++;
-						$gfsolution = $self->add("gapfillingSolutions",{id => $self->id().".gfsol.".$solcount,suboptimal => $args->{subopt}});
-					}
-					my $subarray = [split(/[,;]/,$solutionsArray->[$k])];
-					for (my $j=0; $j < @{$subarray}; $j++) {
-						if ($subarray->[$j] =~ m/([\+])(.+)DrnRxn/) {
-							my $cpdid = $2;
-							my $sign = $1;
-							my $bio = $mdl->biomasses()->[0];
-							my $biocpds = $bio->biomasscompounds();
-							my $found = 0;
-							for (my $m=0; $m < @{$biocpds}; $m++) {
-								my $biocpd = $biocpds->[$m];
-								if ($biocpd->modelcompound()->compound()->id() eq $cpdid) {
-									$found = 1;
-									push(@{$gfsolution->biomassRemovals()},$biocpd->modelcompound());
-									push(@{$gfsolution->biomassRemoval_refs()},$biocpd->modelcompound()->_reference());	
-								}
-							}
-							if ($found == 0) {
-								Bio::KBase::ObjectAPI::utilities::ERROR("Could not find compound to remove from biomass ".$cpdid."!");
-							}
-							$count += 5;
-						} elsif ($subarray->[$j] =~ m/([\-\+])(.+)/) {
-							my $comp = "c";
-							my $rxnid = $2;
-							my $sign = $1;
-							if ($sign eq "+") {
-								$sign = ">";
-							} else {
-								$sign = "<";
-							}
-							my $rxn = $mdl->template()->biochemistry()->queryObject("reactions",{id => $rxnid});
-							if (!defined($rxn)) {
-								Bio::KBase::ObjectAPI::utilities::ERROR("Could not find gapfilled reaction ".$rxnid."!");
-							}
-							my $cmp = $mdl->template()->biochemistry()->queryObject("compartments",{id => $comp});
-							if (!defined($rxn)) {
-								Bio::KBase::ObjectAPI::utilities::ERROR("Could not find gapfilled reaction compartment ".$comp."!");
-							}
-							if (defined($rxnHash->{$rxn->_reference()}->{$cmp->_reference()}) && $rxnHash->{$rxn->_reference()}->{$cmp->_reference()} ne $sign) {
-								$rxnHash->{$rxn->_reference()}->{$cmp->_reference()} = "=";
-							} else {
-								$rxnHash->{$rxn->_reference()}->{$cmp->_reference()} = $sign;
-							}
-							$count++;
-						}
-					}
-					foreach my $ruuid (keys(%{$rxnHash})) {
-						foreach my $cuuid (keys(%{$rxnHash->{$ruuid}})) {
-							$gfsolution->add("gapfillingSolutionReactions",{
-								reaction_ref => $ruuid,
-								compartment_ref => $cuuid,
-								direction => $rxnHash->{$ruuid}->{$cuuid}
-							});
-						}
-					}
-				}
+		my $solutionsArray = [split(/\|/,$array->[1])];   				
+		my $gfsolution;
+		my $count = 0;
+		for (my $k=0; $k < @{$solutionsArray}; $k++) {
+		    if (length($solutionsArray->[$k]) > 0) {
+			my $rxnHash;
+			if ($k == 0 || $self->completeGapfill() ne "1") {
+			    if (defined($gfsolution)) {
+				$gfsolution->solutionCost($count);
+			    }
+			    $count = 0;
+			    $solcount++;
+			    $gfsolution = $self->add("gapfillingSolutions",{id => $self->id().".gfsol.".$solcount,suboptimal => $args->{subopt}});
 			}
-			$gfsolution->solutionCost($count);
+			my $subarray = [split(/[,;]/,$solutionsArray->[$k])];
+			for (my $j=0; $j < @{$subarray}; $j++) {
+			    if ($subarray->[$j] =~ m/([\+])(.+)DrnRxn/) {
+				my $cpdid = $2;
+				my $sign = $1;
+				my $bio = $mdl->biomasses()->[0];
+				my $biocpds = $bio->biomasscompounds();
+				my $found = 0;
+				for (my $m=0; $m < @{$biocpds}; $m++) {
+				    my $biocpd = $biocpds->[$m];
+				    if ($biocpd->modelcompound()->compound()->id() eq $cpdid) {
+					$found = 1;
+					push(@{$gfsolution->biomassRemovals()},$biocpd->modelcompound());
+					push(@{$gfsolution->biomassRemoval_refs()},$biocpd->modelcompound()->_reference());	
+				    }
+				}
+				if ($found == 0) {
+				    Bio::KBase::ObjectAPI::utilities::ERROR("Could not find compound to remove from biomass ".$cpdid."!");
+				}
+				$count += 5;
+			    } elsif ($subarray->[$j] =~ m/([\-\+])(.+)/) {
+				my $comp = "c";
+				my $rxnid = $2;
+				my $sign = $1;
+				if ($sign eq "+") {
+				    $sign = ">";
+				} else {
+				    $sign = "<";
+				}
+				my $rxn = $mdl->template()->biochemistry()->queryObject("reactions",{id => $rxnid});
+				if (!defined($rxn)) {
+				    Bio::KBase::ObjectAPI::utilities::ERROR("Could not find gapfilled reaction ".$rxnid."!");
+				}
+				my $cmp = $mdl->template()->biochemistry()->queryObject("compartments",{id => $comp});
+				if (!defined($rxn)) {
+				    Bio::KBase::ObjectAPI::utilities::ERROR("Could not find gapfilled reaction compartment ".$comp."!");
+				}
+				if (defined($rxnHash->{$rxn->_reference()}->{$cmp->_reference()}) && $rxnHash->{$rxn->_reference()}->{$cmp->_reference()} ne $sign) {
+				    $rxnHash->{$rxn->_reference()}->{$cmp->_reference()} = "=";
+				} else {
+				    $rxnHash->{$rxn->_reference()}->{$cmp->_reference()} = $sign;
+				}
+				$count++;
+			    }
+			}
+			foreach my $ruuid (keys(%{$rxnHash})) {
+			    foreach my $cuuid (keys(%{$rxnHash->{$ruuid}})) {
+				$gfsolution->add("gapfillingSolutionReactions",{
+				    reaction_ref => $ruuid,
+				    compartment_ref => $cuuid,
+				    direction => $rxnHash->{$ruuid}->{$cuuid}
+						 });
+			    }
+			}
+		    }
 		}
-	}
+		$gfsolution->solutionCost($count);
+	} elsif ($has_unneeded) {
+	    # Create an empty solution if all lines were UNNEEDED
+	    $solcount++;
+	    my $gfsolution = $self->add("gapfillingSolutions",{id => $self->id().".gfsol.".$solcount});
+	} # Otherwise we assume that all of the attempts failed.
 }
 
 sub parseGeneCandidates {
