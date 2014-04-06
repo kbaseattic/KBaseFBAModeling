bda9aa53e5c6b81796ba019f945d6c0fd0b534a0
root <root@mattb-dev.novalocal>
2014-04-06 18:49:18 +0000

    Fix GPR generation for gap filled reactions

--- lib/Bio/KBase/ObjectAPI/KBaseFBA/ModelReaction.pm
+++ lib/Bio/KBase/ObjectAPI/KBaseFBA/ModelReaction.pm
@@ -90,12 +90,28 @@ sub _buildmodelCompartmentLabel {
 }
 sub _buildgprString {
 	my ($self) = @_;
-	my $gpr = "";
+	my $gprs = [];
+	my $allUnknown = 1;
 	foreach my $protein (@{$self->modelReactionProteins()}) {
-		if (length($gpr) > 0) {
-			$gpr .= " or ";	
-		}
-		$gpr .= $protein->gprString();
+	    my $one_gpr = $protein->gprString();
+	    if ( $one_gpr ne "Unknown" ) {
+		$allUnknown = 0;
+	    }
+	    push(@$gprs, $protein->gprString());
+	}
+	my $gpr = "";
+	# Account for possibility that all of the multiple reaction proteins are empty.
+	if ( $allUnknown == 1 ) {
+	    $gpr = "Unknown";
+	    return $gpr;
+	}
+	foreach my $one_gpr (@$gprs) {
+	    # Avoid printing GPRs that look like (unknown or GENE) if one modelReactionProtein is empty and another has genes in it.
+	    if ( $one_gpr eq "Unknown" ) { next; }
+	    if (length($gpr) > 0) {
+		$gpr .= " or ";	
+	    }
+	    $gpr .= $one_gpr;
 	}
 	if (@{$self->modelReactionProteins()} > 1) {
 		$gpr = "(".$gpr.")";	
