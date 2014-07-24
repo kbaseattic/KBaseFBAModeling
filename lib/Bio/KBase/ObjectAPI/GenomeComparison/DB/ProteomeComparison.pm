e3553145f8b6cdc8393d4e0289dc8e2c2d1e457c
Christopher Henry <chenry@mcs.anl.gov>
2014-07-24 14:07:13 -0500

    Fixing bug in proteome comparison object

--- lib/Bio/KBase/ObjectAPI/GenomeComparison/DB/ProteomeComparison.pm
+++ lib/Bio/KBase/ObjectAPI/GenomeComparison/DB/ProteomeComparison.pm
@@ -18,10 +18,12 @@ has parent => (is => 'rw', isa => 'Ref', weak_ref => 1, type => 'parent', metacl
 has uuid => (is => 'rw', lazy => 1, isa => 'Str', type => 'msdata', metaclass => 'Typed',builder => '_build_uuid');
 has _reference => (is => 'rw', lazy => 1, isa => 'Str', type => 'msdata', metaclass => 'Typed',builder => '_build_reference');
 has max_evalue => (is => 'rw', isa => 'Str', printOrder => '-1', type => 'attribute', metaclass => 'Typed');
+has proteome1map => (is => 'rw', isa => 'HashRef', printOrder => '-1', default => sub {return {};}, type => 'attribute', metaclass => 'Typed');
 has sub_bbh_percent => (is => 'rw', isa => 'Num', printOrder => '-1', type => 'attribute', metaclass => 'Typed');
 has genome1ref => (is => 'rw', isa => 'Str', printOrder => '0', type => 'attribute', metaclass => 'Typed');
 has genome2id => (is => 'rw', isa => 'Str', printOrder => '-1', type => 'attribute', metaclass => 'Typed');
 has genome2ref => (is => 'rw', isa => 'Str', printOrder => '0', type => 'attribute', metaclass => 'Typed');
+has proteome2map => (is => 'rw', isa => 'HashRef', printOrder => '-1', default => sub {return {};}, type => 'attribute', metaclass => 'Typed');
 has data2 => (is => 'rw', isa => 'ArrayRef', printOrder => '-1', default => sub {return [];}, type => 'attribute', metaclass => 'Typed');
 has id => (is => 'rw', isa => 'Str', printOrder => '-1', type => 'attribute', metaclass => 'Typed');
 has genome1ws => (is => 'rw', isa => 'Str', printOrder => '-1', type => 'attribute', metaclass => 'Typed');
@@ -58,6 +60,14 @@ my $attributes = [
           {
             'req' => 0,
             'printOrder' => -1,
+            'name' => 'proteome1map',
+            'default' => 'sub {return {};}',
+            'type' => 'HashRef',
+            'perm' => 'rw'
+          },
+          {
+            'req' => 0,
+            'printOrder' => -1,
             'name' => 'sub_bbh_percent',
             'type' => 'Num',
             'perm' => 'rw'
@@ -81,6 +91,14 @@ my $attributes = [
             'printOrder' => 0,
             'name' => 'genome2ref',
             'type' => 'Str',
+            'perm' => 'rw'
+          },
+          {
+            'req' => 0,
+            'printOrder' => -1,
+            'name' => 'proteome2map',
+            'default' => 'sub {return {};}',
+            'type' => 'HashRef',
             'perm' => 'rw'
           },
           {
@@ -145,7 +163,7 @@ my $attributes = [
           }
         ];
 
-my $attribute_map = {max_evalue => 0, sub_bbh_percent => 1, genome1ref => 2, genome2id => 3, genome2ref => 4, data2 => 5, id => 6, genome1ws => 7, proteome1names => 8, genome1id => 9, proteome2names => 10, genome2ws => 11, data1 => 12};
+my $attribute_map = {max_evalue => 0, proteome1map => 1, sub_bbh_percent => 2, genome1ref => 3, genome2id => 4, genome2ref => 5, proteome2map => 6, data2 => 7, id => 8, genome1ws => 9, proteome1names => 10, genome1id => 11, proteome2names => 12, genome2ws => 13, data1 => 14};
 sub _attributes {
 	 my ($self, $key) = @_;
 	 if (defined($key)) {
